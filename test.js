c7bd43ac1907efd6db4852e1633655fcac3fdfde
Ethan Taubman <etaubman@gmail.com>
2013-04-27 22:23:56 -0300

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('.test-input').typeintent();
 	$('.test-input').on('typeintent',function(e){
-		var val = $(this).val();
-		console.log( val.match(/\w+/g) );
+		console.log($(this).val());
 	})
 });
